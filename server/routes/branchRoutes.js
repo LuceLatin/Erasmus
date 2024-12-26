c820ae453318b536b36781329bd43ba27e1675de
Nikola Occidentale <nikola.occidentale@stylight.com>
2024-12-26 14:38:36 +0100

    Fix branchRoutes

--- server/routes/branchRoutes.js
+++ server/routes/branchRoutes.js
@@ -6,6 +6,40 @@ import mongoose from "mongoose";
 
 const branchRoutes = express.Router();
 
+branchRoutes.get("/api/branches/grouped", validateToken, checkAuthorization2(['koordinator', 'student', 'profesor']), async (req, res) => {
+    try {
+        const branches = await Branch.find().populate('institution');
+
+        console.log('branches:', branches);
+        const groupedBranches = groupBranchesByInstitution(branches);
+
+        res.json(Object.values(groupedBranches));
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch branches.' });
+    }
+});
+
+branchRoutes.get("/api/branches/grouped/:id", validateToken, checkAuthorization2(['koordinator', 'student', 'profesor']), async (req, res) => {
+    try {
+        const branchId = req.params.id;
+        const userBranch = await Branch.findById(branchId);
+
+        const branches = await Branch.find({ category: userBranch.category }).populate('institution');
+
+        if (branches.length === 0) {
+            return res.status(404).json({ error: 'No branches found for the specified category.' });
+        }
+
+        console.log('branches:', branches);
+        const groupedBranches = groupBranchesByInstitution(branches);
+
+        res.json(Object.values(groupedBranches));
+    } catch (error) {
+        console.error('Error fetching branches:', error);
+        res.status(500).json({ error: 'Failed to fetch branches.' });
+    }
+});
+
 
 branchRoutes.get("/api/branches/:id", validateToken, checkAuthorization2(['koordinator', 'student', 'profesor']), async (req, res) => {
     try {
@@ -88,66 +122,34 @@ branchRoutes.post("/api/branches/add", checkAuthorization, async (req, res) => {
     }
 });
 
-//Nikola ne znam cemu ovo
 
-// const groupBranchesByInstitution = (branches) => {
-//     return branches.reduce((acc, branch) => {
-//         const institutionId = branch.institution._id;
-//         if (!acc[institutionId]) {
-//             acc[institutionId] = {
-//                 institution: branch.institution,
-//                 branches: []
-//             };
-//         }
-//         const { institution, ...branchWithoutInstitution } = branch._doc;
-//         acc[institutionId].branches.push(branchWithoutInstitution);
-//         return acc;
-//     }, {});
-// };
+const groupBranchesByInstitution = (branches) => {
+    return branches.reduce((acc, branch) => {
+        const institutionId = branch.institution._id;
+        if (!acc[institutionId]) {
+            acc[institutionId] = {
+                institution: branch.institution,
+                branches: []
+            };
+        }
+        const { institution, ...branchWithoutInstitution } = branch._doc;
+        acc[institutionId].branches.push(branchWithoutInstitution);
+        return acc;
+    }, {});
+};
 
-// branchRoutes.get("/api/branches", validateToken, checkAuthorization2(['koordinator', 'student', 'profesor']), async (req, res) => {
-//     try {
-//         let branches;
-//         if (req.query.categoryId) {
-//             branches = await Branch.find({ category: req.query.categoryId }).populate('institution');
-//         } else {
-//             branches = await Branch.find().populate('institution');
-//         }
-//         res.json(branches);
-//     } catch (error) {
-//         res.status(500).json({ error: 'Failed to fetch branches.' });
-//     }
-// });
+branchRoutes.get("/api/branches", validateToken, checkAuthorization2(['koordinator', 'student', 'profesor']), async (req, res) => {
+    try {
+        let branches;
+        if (req.query.categoryId) {
+            branches = await Branch.find({ category: req.query.categoryId }).populate('institution');
+        } else {
+            branches = await Branch.find().populate('institution');
+        }
+        res.json(branches);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch branches.' });
+    }
+});
 
-// branchRoutes.get("/api/branches/grouped", validateToken, checkAuthorization2(['koordinator', 'student', 'profesor']), async (req, res) => {
-//     try {
-//         const branches = await Branch.find().populate('institution');
-
-//         const groupedBranches = groupBranchesByInstitution(branches);
-
-//         res.json(Object.values(groupedBranches));
-//     } catch (error) {
-//         res.status(500).json({ error: 'Failed to fetch branches.' });
-//     }
-// });
-
-// branchRoutes.get("/api/branches/grouped/:id", validateToken, checkAuthorization2(['koordinator', 'student', 'profesor']), async (req, res) => {
-//     try {
-//         const branchId = req.params.id;
-//         const userBranch = await Branch.findById(branchId);
-
-//         const branches = await Branch.find({ category: userBranch.category }).populate('institution');
-
-//         if (branches.length === 0) {
-//             return res.status(404).json({ error: 'No branches found for the specified category.' });
-//         }
-
-//         const groupedBranches = groupBranchesByInstitution(branches);
-
-//         res.json(Object.values(groupedBranches));
-//     } catch (error) {
-//         console.error('Error fetching branches:', error);
-//         res.status(500).json({ error: 'Failed to fetch branches.' });
-//     }
-// });
 export default branchRoutes;
